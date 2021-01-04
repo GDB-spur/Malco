548b79ecbe14eeb3304a5008e80e795a061bd492
GDB-spur <gdelbene22@weldre4.org>
2021-01-03 21:53:57 -0700

    Added some new functions to BasicCommands as well as a new system for which function to send the message.

--- Source/BasicCommands.asm
+++ Source/BasicCommands.asm
@@ -1,22 +1,31 @@
 ;Testing program
 ;Spur
 ;.386
-;.model flat,stdcall
+;.model small,c
 ;.stack 4096
 
 .data
+;Calling certain variables from C
+
+  fmtlist   db     "%s, %d, %lu", 0Ah,0
+  string_1  db     "signed byte and unsigned double word", 0
+  data_1    db     -2
+  data_2    dd     0FFFFFFFFh
+
+ ;Functions
 
 MSG			DB 'MASM', 0
-MONE		DB 'Brayconn, '
-MTWO		DB 'Txin, Smudge, Kayo, Spur, '
+MONE		DB 'Brayconn, ', 0
+MTWO		DB 'Txin, Smudge, Kayo, Spur, ', 0
 MTHREE		DB 'how are you?', 0
 
 .code
 public OnMessageASM
 OnMessageASM PROC
-	mov rax, offset MONE
-	;mov rax, offset MTWO
-	;mov rax, offset MTHREE
+	inc RAX
+	mov RAX, offset MONE
+	cmp rax, 1
+	mov rax, offset MTWO
 	ret
 OnMessageASM endp
 
